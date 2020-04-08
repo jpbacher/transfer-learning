a970e6a4849a91840a1a63b66d156d71a392a00b
jpbacher <bacherjp@gmail.com>
2020-04-08 14:19:38 -0400

    added resnet50 model

--- data/data.py
+++ data/data.py
@@ -1,16 +1,12 @@
 import glob
-import numpy as np
-import torch
 from PIL import Image
 from torch.utils.data import Dataset
-from torch.utils.data.sampler import SubsetRandomSampler
-from torchvision import datasets, transforms
 
 
 class CucumberZucchini(Dataset):
-    def __init__(self, data_root, transforms=None):
-        self.data_root = data_root
-        self.img_files = [img for img in glob.glob(self.data_root + '**/*/**/*', recursive=True)]
+    def __init__(self, data_dir, transforms=None):
+        self.data_dir = data_dir
+        self.img_files = [img for img in glob.glob(self.data_dir + '**/*/**/*', recursive=True)]
         self.transforms = transforms
 
     def __len__(self):
@@ -20,28 +16,3 @@ class CucumberZucchini(Dataset):
         print(f'Retrieving image {idx}')
         image = Image.open(self.img_files[idx])
         return image
-
-     def _get_loaders(self, data_dir, val_size, transforms):
-        train_sample, val_sample = self._get_samplers(val_size, transforms)
-        train_loader = torch.utils.data.DataLoader(
-            self.train_data, batch_size=8, sampler=train_sample)
-        val_loader = torch.utils.data.DataLoader(
-            self.val_data, batch_size=8, sampler=val_sample)
-        return train_loader, val_loader
-
-    def _get_samplers(self, val_size, transforms):
-        np.random.seed(30)
-        train_count = len(self.train_data)
-        indices = list(range(train_count))
-        split = int(train_count * val_size)
-        np.random.shuffle(indices)
-        train_idx, val_idx = indices[split:], indices[:split]
-        train_sample = SubsetRandomSampler(train_idx)
-        val_sample = SubsetRandomSampler(val_idx)
-        return train_sample, val_sample
-
-    self.train_data = datasets.ImageFolder(self.data_root, transforms)
-    self.val_data = datasets.ImageFolder(self.data_root, transforms)
-    self.train_loader, self.val_loader = self._get_loaders(self.img_files, self.val_size, transforms)
-
-    def train_model(self, model, loaders, loss_fn, optimizer, epochs):
---
+++ models/resnet.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+from torchvision.models import resnet50
+
+
+def resnet_finetune(model):
+    for param in model.parameters():
+        param.requires_grad = False
+    model.fc = nn.Sequential(nn.Linear(2048, 512),
+                             nn.ReLU(),
+                             nn.BatchNorm2d(512)
+                             nn.Linear(512, 1),
+                             nn.Sigmoid())
+    return model
+
+
+resnet50 = resnet_finetune(resnet50(pretrained=True))
