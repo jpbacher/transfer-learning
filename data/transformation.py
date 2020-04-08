cd435fcc15c6cc64bbfa3b4baacb8a21ee34575e
jpbacher <bacherjp@gmail.com>
2020-04-08 08:04:20 -0400

    added transformations

---
+++ data/transformation.py
@@ -0,0 +1,18 @@
+import numpy as np
+import torchvision.transforms as T
+
+
+data_transforms = {
+    'train': T.Compose([
+        T.RandomResizedCrop(224),
+        T.RandomHorizontalFlip(),
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': T.Compose([
+        T.Resize(256),
+        T.CenterCrop(224),
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
