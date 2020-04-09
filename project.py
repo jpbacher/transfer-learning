d33eaf8a974f33878134cb9531e6f331d0fae724
jpbacher <bacherjp@gmail.com>
2020-04-09 11:42:11 -0400

    fixed typo

--- project.py
+++ project.py
@@ -5,9 +5,9 @@ from pathlib import Path
 @dataclass
 class Project:
     base_dir: Path = Path(__file__).parents[0]
-    data_dir: base_dir / 'dataset'
-    checkpoint_dir : base_dir / 'checkpoint'
+    data_dir = base_dir / 'dataset'
+    checkpoint_dir = base_dir / 'checkpoint'
 
-    def __phost_init__(self):
+    def __post_init__(self):
         self.data_dir.mkdir(exist_ok=True)
         self.checkpoint_dir.mkdir(exist_ok=True)
