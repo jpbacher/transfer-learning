d2ad9506e77ec527a044b384940efc72c1a1fcb8
jpbacher <bacherjp@gmail.com>
2020-04-10 11:39:23 -0400

    added a test loader

--- data/__init__.py
+++ data/__init__.py
@@ -10,28 +10,31 @@ def get_loaders(
         data_dir,
         train_transforms=None,
         val_transforms=None,
-        val_size=0.2,
-        batch_size=16):
+        train_test_split=0.85,
+        train_val_split=0.15,
+        batch_size=batch_size,
+        shuffle=True):
     """
-    This function returns the training & validation loaders.
-
-    :param data_dir:
-    :param train_transforms:
-    :param val_transforms:
-    :param val_size:
-    :param batch_size:
-    :return loaders
+    This function returns the training, validation, & test loaders.
     """
     np.random.seed(24)
-    train_data = ImageFolder(root=data_dir, transform=train_transforms)
-    val_data = ImageFolder(root=data_dir, transform=val_transforms)
-    length = len(train_data)
-    indices = list(range(length))
-    split = int(length * val_size)
-    np.random.shuffle(indices)
-    train_idx, val_idx = indices[split:], indices[:split]
-    train_sample, val_sample = SubsetRandomSampler(train_idx), SubsetRandomSampler(val_idx)
-    train_loader = DataLoader(train_data, batch_size=batch_size, sampler=train_sample)
-    val_loader = DataLoader(val_data, batch_size=batch_size, sampler=val_sample)
+    train_ds = ImageFolder(root=data_dir, transform=train_transforms)
+    val_ds = ImageFolder(root=data_dir, transform=val_transforms)
+    test_ds = ImageFolder(root=data_dir, transform=val_transforms)
+    img_count = len(train_ds)
+    indices = list(range(img_count))
+    test_split = int(img_count * train_test_split)
+    if shuffle:
+        np.random.shuffle(indices)
+    train_idx, test_idx = indices[:test_split], indices[test_split:]
+    train_count = len(train_idx)
+    val_split = int(train_count * (1 - train_val_split))
+    train_idx, val_idx = train_idx[:val_split], train_idx[val_split:]
+    train_sample = SubsetRandomSampler(train_idx)
+    val_sample = SubsetRandomSampler(val_idx)
+    test_sample = SubsetRandomSampler(test_idx)
+    train_loader = DataLoader(train_ds, batch_size=batch_size, sampler=train_sample)
+    val_loader = DataLoader(val_ds, batch_size=batch_size, sampler=val_sample)
+    test_loader = DataLoader(test_ds, batch_size=batch_size, sampler=test_sample)
 
-    return train_loader, val_loader
+    return train_loader, val_loader, test_loader
