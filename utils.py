a51bb0f825c02c2ab297a13229dfe09eb9f9e1e8
jpbacher <bacherjp@gmail.com>
2020-04-09 20:24:50 -0400

    fixed definition

--- utils.py
+++ utils.py
@@ -7,13 +7,5 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def show_img(img):
-    img = img.numpy()
-    plt.imshow(np.transpose(img, (1, 2, 0)), interpolation='nearest')
-    plt.show()
-
-def show_batch(dataset, n=10):
-    imgs = [dataset[img][0] for img in range(n)]
-    grid = make_grid(imgs)
-    plt.imshow(grid.numpy().transpose((1, 2, 0)))
-    plt.tight_layout()
-    plt.show()
+    img = plt.imread(img)
+    plt.imshow(img)
