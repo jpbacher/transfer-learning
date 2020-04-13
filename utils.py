7085bef97dbc952194527d6a8a0ace38161eb2aa
jpbacher <bacherjp@gmail.com>
2020-04-13 07:18:32 -0400

    added function

--- utils.py
+++ utils.py
@@ -9,3 +9,13 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 def show_img(img):
     img = plt.imread(img)
     plt.imshow(img)
+
+
+def plot_loss_results(history):
+    for loss in ['train_loss', 'val_loss']:
+        plt.figure(figsize=(10, 8))
+        plt.plot(history[loss], label=loss)
+        plt.legend()
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.show()
