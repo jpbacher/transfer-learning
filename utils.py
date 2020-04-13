4c7eb273d5b8ef6dfcf1a3e167f45b20bfc60385
jpbacher <bacherjp@gmail.com>
2020-04-13 07:38:37 -0400

    added function

--- utils.py
+++ utils.py
@@ -12,10 +12,22 @@ def show_img(img):
 
 
 def plot_loss_results(history):
+    plt.figure(figsize=(10, 8))
     for loss in ['train_loss', 'val_loss']:
-        plt.figure(figsize=(10, 8))
         plt.plot(history[loss], label=loss)
-        plt.legend()
-        plt.xlabel('Epochs')
-        plt.ylabel('Loss')
-        plt.show()
+    plt.legend()
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Training vs Validation Loss')
+    plt.show()
+
+
+def plot_accuracy_results(history):
+    plt.figure(figsize=(10, 8))
+    for acc in ['train_acc', 'val_acc']:
+        plt.plot(history[acc], label=acc)
+    plt.legend()
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.title('Training vs Validation Accuracy')
+    plt.show()
