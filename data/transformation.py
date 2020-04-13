65f80c6da7fb19d12ab20a983375f32b6df1bb7c
jpbacher <bacherjp@gmail.com>
2020-04-12 21:47:37 -0400

    added early stopping

--- data/transformation.py
+++ data/transformation.py
@@ -4,8 +4,11 @@ import torchvision.transforms as T
 
 data_transforms = {
     'train': T.Compose([
+        T.RandomResizedCrop(size=256, scale=(0.8,1.0)),
+        T.RandomRotation(degrees=15),
+        T.ColorJitter(),
+        T.RandomHorizontalFlip(p=0.5),
         T.RandomResizedCrop(224),
-        T.RandomHorizontalFlip(),
         T.ToTensor(),
         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
