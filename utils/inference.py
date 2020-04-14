5427565e674bf78b3d3b060b372d5f64a2af57c8
jpbacher <bacherjp@gmail.com>
2020-04-14 14:56:37 -0400

    fixed predict function

--- utils/inference.py
+++ utils/inference.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import torch
 from torchvision.datasets.folder import ImageFolder
 from PIL import Image
 
@@ -47,12 +48,11 @@ def process_to_tensor(img_path):
 
 def predict(data_dir, img_path, model, top_k=2):
     """
-    Make prediction on an unseen image using pre-trained model.
-    :param data_dir: directory where images located
-    :param img_path: filename on image
-    :param model: PyTorch model for inference
-    :param top_k: the number of top predictions to return
-    :return: top probabilities & top classes
+    :param data_dir: directory where images are located
+    :param img_path: path to specific image
+    :param model: model that has been trained
+    :param top_k: number of top classes/labels to return
+    :return: the image & the probabilities & labels model predicts
     """
     img_tensor = process_to_tensor(img_path)
     img_tensor = img_tensor.view(1, 3, 224, 224)
@@ -66,17 +66,17 @@ def predict(data_dir, img_path, model, top_k=2):
             model.idx_to_class[label] for label in top_labels.numpy()[0]
         ]
         top_prob = topk.numpy()[0]
-    return img_tensor, top_prob, top_labels
+    return img_tensor.cpu().squeeze(), top_prob, top_labels
 
 
 def plot_prediction(data_dir, img_path, model, topk=3):
     """
-    Plot the predicted probabilities from the model and display actual image.
-    :param data_dir: directory where images located
-    :param img_path: path to test image
-    :param model: a trained model
-    :param topk: top predicted labels to display
-    :return: the test image and plot of probabilities
+    Show the image & plot the top probabilities that the model predicts
+    :param data_dir: directory where images are located
+    :param img_path: path to specific image
+    :param model: model that has been trained
+    :param topk: number of top classes/labels to plot
+    :return: actual test image & plot of top probabilities
     """
     img, top_prob, top_labels = predict(
         data_dir, img_path, model, topk)
