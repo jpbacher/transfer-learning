c4cf262e74a940f0289a72efef49bf0ee628851c
jpbacher <bacherjp@gmail.com>
2020-04-14 10:50:33 -0400

    added CenterCrop to train transformation

--- data/transformation.py
+++ data/transformation.py
@@ -8,7 +8,7 @@ data_transforms = {
         T.RandomRotation(degrees=15),
         T.ColorJitter(),
         T.RandomHorizontalFlip(p=0.5),
-        T.RandomResizedCrop(224),
+        T.CenterCrop(224),
         T.ToTensor(),
         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
